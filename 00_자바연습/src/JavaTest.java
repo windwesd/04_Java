21e8eb47bbf68d124d52ccda9fb7b6340e05704d
windwesd <hjkims1122@naver.com>
2024-07-15 17:10:27 +0900

    20240715_Java 시작, 변수, print 구문

---
+++ 00_자바연습/src/JavaTest.java
@@ -0,0 +1,25 @@
+// 주석은 JS와 동일
+
+// 한 줄 주석
+/* 범위 주석 */
+
+// ctrl + +/- : 글씨 크기 조절
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		System.out.println("이클립스 사용 중");
+		
+		// sysout, ctrl + space -> enter
+		System.out.println("메모장 보다 쉽다");
+		
+		// 실행 : 위에 Run 버튼 또는 단축키 ctrl + F11
+		
+		/* Java API DOC 확인 방법
+		 *  - Java API : 자바가 만들어서 제공해준 코드(기능)
+		 *  - API DOC  : 제공하는 기능에 대한 설명 문서
+		 *  
+		 *  궁금한 것 클릭 -> shift + F2
+		 * */
+	}
+}
