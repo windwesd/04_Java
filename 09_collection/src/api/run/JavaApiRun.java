b536a92a23b55f9fd951786d4728957a1a745a5b
windwesd <hjkims1122@naver.com>
2024-07-30 17:49:28 +0900

    20240730 커밋

---
+++ 09_collection/src/api/run/JavaApiRun.java
@@ -0,0 +1,26 @@
+package api.run;
+
+import api.service.JavaApiService;
+
+public class JavaApiRun {
+	public static void main(String[] args) {
+		JavaApiService service = new JavaApiService();
+		
+//		service.test1();
+//		service.test2();
+//		service.test3();
+		service.test4();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ 10_exception/src/pkg2/service/TryCatchFinallyRun.java
@@ -0,0 +1,11 @@
+package pkg2.service;
+
+public class TryCatchFinallyRun {
+	public static void main(String[] args) {
+		TryCatchFinallyService service = new TryCatchFinallyService();
+		
+//		service.test1();
+		service.test2();
+		
+	}
+}
