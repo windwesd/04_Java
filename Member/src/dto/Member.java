5edfb4d19d8a0d07a27bbee329ce95e703a54658
windwesd <hjkims1122@naver.com>
2024-08-01 17:35:13 +0900

    20240801 회원관리프로그램 커밋

---
+++ Member/src/dto/Member.java
@@ -0,0 +1,26 @@
+package dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member implements Serializable {
+	public static final int COMMON = 0;
+	public static final int GOLD = 1;
+	public static final int DIAMOND = 2;
+
+	// 회원 정보를 저장할 필드
+	private String name;
+	private String Phone;
+	private int amount;
+	private int grade;
+}
